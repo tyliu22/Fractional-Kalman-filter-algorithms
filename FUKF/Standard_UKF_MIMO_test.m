c117219ccf8c79887f6747ab3ceebd09840f5811
Github <78165038+MYGITHUBWHO@users.noreply.github.com>
2021-05-05 04:37:55 +0200

    Save

--- FUKF/Standard_UKF_MIMO_test.m
+++ FUKF/Standard_UKF_MIMO_test.m
@@ -1,55 +1,55 @@
 %*************************************************************************%
-%                      ÎŞ¼£¿¨¶ûÂüÂË²¨Æ÷·ÂÕæ¸´ÏÖ                           %
+%                      æ— è¿¹å¡å°”æ›¼æ»¤æ³¢å™¨ä»¿çœŸå¤ç°                           %
 %_________________________________________________________________________%
-%   ÂÛÎÄ : ·ÇÏßĞÔÏµÍ³ÂË²¨ÀíÂÛ P108 5.4.2 Ç¿·ÇÏßĞÔÏµÍ³Ä£ĞÍ·ÂÕæ
-%   Ä¿µÄ : ÎŞ¼£¿¨¶ûÂüÂË²¨Æ÷·ÂÕæ¸´ÏÖ
-%   º¯ÊıÊµÑé ::
+%   è®ºæ–‡ : éçº¿æ€§ç³»ç»Ÿæ»¤æ³¢ç†è®º P108 5.4.2 å¼ºéçº¿æ€§ç³»ç»Ÿæ¨¡å‹ä»¿çœŸ
+%   ç›®çš„ : æ— è¿¹å¡å°”æ›¼æ»¤æ³¢å™¨ä»¿çœŸå¤ç°
+%   å‡½æ•°å®éªŒ ::
 %                   | 3*sin(2*x_2)                 |   | 1 |
 %             x_k = | x_1 + exp(-0.05x_3) + 10     | + | 1 |w_k
 %                   | = x_1(x_2 + x_3)/5 + |x_1|/2 |   | 1 |
 %
-%   ½á¹û : ÂË²¨Ğ§¹ûÁ¼ºÃ
+%   ç»“æœ : æ»¤æ³¢æ•ˆæœè‰¯å¥½
 %
-%   ±¸×¢ : 
+%   å¤‡æ³¨ : 
 %
 %*************************************************************************%
 
 clc
 clear
 
-%·ÂÕæ²½³¤
+%ä»¿çœŸæ­¥é•¿
 N = 50;
 
-q = 0.3;                %ÏµÍ³ÔëÉù¾ùÖµ
-r = 0.5;                %²âÁ¿ÔëÉù¾ùÖµ
-Q = 0.7;                %ÏµÍ³ÔëÉù·½²î¾ØÕó
-R = 1.0;                %²âÁ¿ÔëÉù·½²î¾ØÕó
+q = 0.3;                %ç³»ç»Ÿå™ªå£°å‡å€¼
+r = 0.5;                %æµ‹é‡å™ªå£°å‡å€¼
+Q = 0.7;                %ç³»ç»Ÿå™ªå£°æ–¹å·®çŸ©é˜µ
+R = 1.0;                %æµ‹é‡å™ªå£°æ–¹å·®çŸ©é˜µ
 
-% %GL¶¨ÒåÏÂ¶Ì¼ÇÒäÔ­ÀíµÄ³¤¶È
+% %GLå®šä¹‰ä¸‹çŸ­è®°å¿†åŸç†çš„é•¿åº¦
 % L = N+1;
 
-% %¼ÆËãalpha½×´Î¶ÔÓ¦µÄGL¶¨ÒåÏµÊı binomial coefficient 
-% bino_fir = zeros(1,N);       %Î¢·Ö½×´ÎÎª0.7Ê±GL¶¨ÒåÏÂµÄÏµÊı
+% %è®¡ç®—alphaé˜¶æ¬¡å¯¹åº”çš„GLå®šä¹‰ç³»æ•° binomial coefficient 
+% bino_fir = zeros(1,N);       %å¾®åˆ†é˜¶æ¬¡ä¸º0.7æ—¶GLå®šä¹‰ä¸‹çš„ç³»æ•°
 % alpha = 0.7;
 % bino_fir(1,1) = 1;
 % for i = 2:1:N
 %     bino_fir(1,i) = (1-(alpha+1)/(i-1))*bino_fir(1,i-1);  
 % end
 
-I = eye(3,3);                %Éú³Éµ¥Î»Õó
+I = eye(3,3);                %ç”Ÿæˆå•ä½é˜µ
 
 %err_state_FEKF  = zeros(kk_N,N);
 
-X_state_real = zeros(3,N);         %ÕæÊµ×´Ì¬
-Z_state_meas = zeros(1,N);         %Êµ¼Ê¹Û²âÖµ
+X_state_real = zeros(3,N);         %çœŸå®çŠ¶æ€
+Z_state_meas = zeros(1,N);         %å®é™…è§‚æµ‹å€¼
 
-%ÔëÉù
-W_noise = sqrt(Q)*randn(1,N) + q;  %ÏµÍ³ÔëÉù
-V_noise = sqrt(R)*randn(1,N) + r;  %²âÁ¿ÔëÉù
+%å™ªå£°
+W_noise = sqrt(Q)*randn(1,N) + q;  %ç³»ç»Ÿå™ªå£°
+V_noise = sqrt(R)*randn(1,N) + r;  %æµ‹é‡å™ªå£°
 
-x_0  = [-0.7; 1; 1];               %³õÊ¼×´Ì¬     
-X_state_real(:,1) = x_0;           %ÕæÊµ×´Ì¬³õÊ¼Öµ
-Z_state_meas(1,1) = V_noise(1,1);  %²âÁ¿Êı¾İ³õÊ¼Öµ
+x_0  = [-0.7; 1; 1];               %åˆå§‹çŠ¶æ€     
+X_state_real(:,1) = x_0;           %çœŸå®çŠ¶æ€åˆå§‹å€¼
+Z_state_meas(1,1) = V_noise(1,1);  %æµ‹é‡æ•°æ®åˆå§‹å€¼
 
 f=@(x)[3*sin(2*x(2)); ...
        x(1) + exp(-0.05*x(3)) + 10; ...
@@ -58,23 +58,23 @@ f=@(x)[3*sin(2*x(2)); ...
 h=@(x)x(1) + x(2) * x(3);
    
 for k=2:1:N
-    %¼ÆËãÊµ¼Ê×´Ì¬
+    %è®¡ç®—å®é™…çŠ¶æ€
     X_state_real(:,k) = f(X_state_real(:,k-1)) + [1; 1; 1] * W_noise(1,k-1);
         
-    %Êµ¼Ê¹Û²âÖµ
+    %å®é™…è§‚æµ‹å€¼
     Z_state_meas(1,k) = h(X_state_real(:,k)) + V_noise(1,k); 
 end
 
 %*************************************************************************%
-%-------------------------ÎŞ¼£¿¨¶ûÂüÂË²¨Æ÷ĞÔÄÜ²âÊÔ------------------------%
+%-------------------------æ— è¿¹å¡å°”æ›¼æ»¤æ³¢å™¨æ€§èƒ½æµ‹è¯•------------------------%
 %*************************************************************************%
 
-X_state_esti = zeros(3,N);      %×´Ì¬×îÓÅ¹À¼ÆÖµ
-P_xesti      = cell(1,N);      %¹À¼ÆÎó²î·½²îÕó
+X_state_esti = zeros(3,N);      %çŠ¶æ€æœ€ä¼˜ä¼°è®¡å€¼
+P_xesti      = cell(1,N);      %ä¼°è®¡è¯¯å·®æ–¹å·®é˜µ
 
-%³õÊ¼ÖµÉèÖÃ£¨³õÊ¼¾ØÕó²»ÄÜÎªÁã£©
-P_pred_0     = eye(3,3);        %³õÊ¼Ô¤²â·½²îÕó
-P_xesti{1,1} = P_pred_0;        %³õÊ¼¹À¼Æ·½²îÕó
+%åˆå§‹å€¼è®¾ç½®ï¼ˆåˆå§‹çŸ©é˜µä¸èƒ½ä¸ºé›¶ï¼‰
+P_pred_0     = eye(3,3);        %åˆå§‹é¢„æµ‹æ–¹å·®é˜µ
+P_xesti{1,1} = P_pred_0;        %åˆå§‹ä¼°è®¡æ–¹å·®é˜µ
 
 state_dim = 3;
 L_sample  = 2 * state_dim +1;
@@ -85,7 +85,7 @@ GammaPoints = zeros(3, L_sample);
 ChiPoints   = zeros(1, L_sample);
 
 for k=2:1:N
-    %¶Ô³Æ²ÉÑù
+    %å¯¹ç§°é‡‡æ ·
     [SigmaWeight, SigmaPoints] = ukf_sample(state_dim, X_state_esti(:,k-1), P_xesti{1,k-1}); 
     
     for i = 1 : 1 : L_sample
@@ -119,7 +119,7 @@ for k=2:1:N
 
     % Predicted measurement error covariance 
     P_zpre = R;
-    for i = 1 : 1 : L_sample 
+    for i = 1 : 5Ã¸ : L_sample 
         P_zpre = P_zpre + SigmaWeight(:,i)*(ChiPoints(:,i)-Z_pre)* ...
                  (ChiPoints(:,i)-Z_pre)';
     end
@@ -144,9 +144,9 @@ end
 
 
 %*******************************%
-%   »­Í¼Êä³ö ¾ùÖµ·½²î¹À¼ÆÉ¢µãÍ¼
+%   ç”»å›¾è¾“å‡º å‡å€¼æ–¹å·®ä¼°è®¡æ•£ç‚¹å›¾
 %*******************************%
-%ÊäÈëÓë²âÁ¿Êä³öÍ¼
+%è¾“å…¥ä¸æµ‹é‡è¾“å‡ºå›¾
 k = 1:1:N;
 
 LineWidth = 2;
@@ -155,12 +155,12 @@ LineWidth = 2;
 figure;
 plot(k,X_state_real(3,:),'r',k,X_state_esti(3,:),'b--','linewidth',LineWidth);
 %set(gcf,'Position',[200 200 400 300]); 
-%axis([xmin xmax ymin ymax])ÉèÖÃ×ø±êÖáÔÚÖ¸¶¨µÄÇø¼ä
+%axis([xmin xmax ymin ymax])è®¾ç½®åæ ‡è½´åœ¨æŒ‡å®šçš„åŒºé—´
 % axis normal
 % axis([ -10 N 0 6 ])
 ylabel('$x_3$','FontSize',8)
 xlabel('time(sec)','FontSize',8)
-%ÉèÖÃ×ø±êÖá¿Ì¶È×ÖÌåÃû³Æ£¬´óĞ¡
+%è®¾ç½®åæ ‡è½´åˆ»åº¦å­—ä½“åç§°ï¼Œå¤§å°
 set(gca,'FontName','Helvetica','FontSize',8)
 legend('real state','estimated state','Location','best');
 legend('Real state 3','Estimation state 3','Location','best');
@@ -168,12 +168,12 @@ legend('Real state 3','Estimation state 3','Location','best');
 figure;
 plot(k,X_state_real(2,:),'r',k,X_state_esti(2,:),'b--','linewidth',LineWidth);
 %set(gcf,'Position',[200 200 400 300]); 
-%axis([xmin xmax ymin ymax])ÉèÖÃ×ø±êÖáÔÚÖ¸¶¨µÄÇø¼ä
+%axis([xmin xmax ymin ymax])è®¾ç½®åæ ‡è½´åœ¨æŒ‡å®šçš„åŒºé—´
 % axis normal
 % axis([ -10 N 0 6 ])
 ylabel('$x_3$','FontSize',8)
 xlabel('time(sec)','FontSize',8)
-%ÉèÖÃ×ø±êÖá¿Ì¶È×ÖÌåÃû³Æ£¬´óĞ¡
+%è®¾ç½®åæ ‡è½´åˆ»åº¦å­—ä½“åç§°ï¼Œå¤§å°
 set(gca,'FontName','Helvetica','FontSize',8)
 legend('real state','estimated state','Location','best');
 legend('Real state 2','Estimation state 2','Location','best');
@@ -182,12 +182,12 @@ legend('Real state 2','Estimation state 2','Location','best');
 figure;
 plot(k,X_state_real(1,:),'r',k,X_state_esti(1,:),'b--','linewidth',LineWidth);
 %set(gcf,'Position',[200 200 400 300]); 
-%axis([xmin xmax ymin ymax])ÉèÖÃ×ø±êÖáÔÚÖ¸¶¨µÄÇø¼ä
+%axis([xmin xmax ymin ymax])è®¾ç½®åæ ‡è½´åœ¨æŒ‡å®šçš„åŒºé—´
 % axis normal
 % axis([ -10 N 0 6 ])
 ylabel('$x_3$','FontSize',8)
 xlabel('time(sec)','FontSize',8)
-%ÉèÖÃ×ø±êÖá¿Ì¶È×ÖÌåÃû³Æ£¬´óĞ¡
+%è®¾ç½®åæ ‡è½´åˆ»åº¦å­—ä½“åç§°ï¼Œå¤§å°
 set(gca,'FontName','Helvetica','FontSize',8)
 legend('real state','estimated state','Location','best');
 legend('Real state 1','Estimation state 1','Location','best');
